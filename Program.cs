c17ce401bb54461fb50c50b13df9c8d3bc7208c6
Stanislav Kuzminov <kuzminoff.stas@gmail.com>
2020-11-16 00:35:02 +0300

    migration actions according to ms guide completed, textcopy nuget package doesn't seem to work though

--- Program.cs
+++ Program.cs
@@ -1,10 +1,7 @@
 using System;
 using System.Net.Http;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using TextCopy;
@@ -16,10 +13,12 @@ namespace RetryCalculator
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+            builder.RootComponents.Add<App>("#app");
 
+            builder.Logging.SetMinimumLevel(LogLevel.Trace);
             builder.Services.InjectClipboard();
-            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(
+                sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
 
             await builder.Build().RunAsync();
         }
